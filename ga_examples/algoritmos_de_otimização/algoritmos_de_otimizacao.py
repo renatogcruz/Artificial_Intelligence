5213d9c7f34ff639d34a80c698564e494335b305
renatogcruz <renatogcruz@hotmail.com>
2020-01-08 08:44:20 -0300

    aulas 18, 19 e 20

--- ga_examples/algoritmos_de_otimização/algoritmos_de_otimizacao.py
+++ ga_examples/algoritmos_de_otimização/algoritmos_de_otimizacao.py
@@ -123,3 +123,35 @@ def subida_encosta(dominio, funcao_custo):
 		if melhor == atual:
 			break
 	return solucao
+
+#solucao_subida_encosta = subida_encosta(dominio, funcao_custo)
+#custo_subida_encosta = funcao_custo(solucao_subida_encosta)
+#imprimir_agenda(solucao_subida_encosta)
+
+def tempera_simulada(dominio, funcao_custo, temperatura = 10000.0, restriamento = 0.95, passo = 1):
+	solucao = [random.randint(dominio[i][0], dominio[i][1]) for i in range(len(dominio))]
+
+	while temperatura > 0.1:
+		i = random.randint(0, len(dominio) - 1)
+		direcao = random.randint(-passo, passo)
+
+		solucao_temp = solucao[:]
+		solucao_temp[i] += direcao
+		if solucao_temp[i] < dominio[i][0]:
+			solucao_temp[i] = dominio[i][0]
+		elif solucao_temp[i] > dominio[i][1]:
+			solucao_temp[i] = dominio[1]
+
+		custo_solucao = funcao_custo(solucao)
+		custo_solucao_temp = funcao_custo(solucao_temp)
+		probabilidade = pow(math.e, (-custo_solucao_temp - custo_solucao) / temperatura)
+
+		if (custo_solucao_temp < custo_solucao or random.random() < probabilidade):
+			solucao = solucao_temp
+
+		temperatura = temperatura * restriamento
+	return solucao
+
+#solucao_tempera_simulada = tempera_simulada(dominio, funcao_custo)
+#custo_subida_encosta = funcao_custo(solucao_tempera_simulada)
+#imprimir_agenda(solucao_tempera_simulada)
